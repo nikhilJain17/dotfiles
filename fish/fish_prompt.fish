1da527c9cf4adf8ea9f9ec5258317012326dea5a
nikhil.jain0987@gmail.com <nikhil.jain0987@gmail.com>
2020-04-27 18:40:41 -0700

    fixed line overflow issue

--- fish/fish_prompt.fish
+++ fish/fish_prompt.fish
@@ -1,7 +1,18 @@
-# Customize the terminal prompt
 function fish_prompt
        set_color brblack
-       echo (whoami)'@'(prompt_hostname) '~>' (pwd)
+       set -l p1 (whoami)'@'(prompt_hostname) '~>' (pwd)
+       set -l p1_len (string length (echo $p1))
+
+       # dir is short enough to be printed
+       if test $p1_len -lt $COLUMNS
+            echo (whoami)'@'(prompt_hostname) '~>' (pwd)
+       end
+
+       # if dir is too long, print a shortened version
+       if test $p1_len -ge $COLUMNS
+            echo (whoami)'@'(prompt_hostname) '~>' (prompt_pwd)
+       end
+
        set_color bryellow
        echo 'λ> '
    end
